28885c66403ce2d82719ced036ff00e3a0eca32a
2356899074@qq.com <2356899074@qq.com>
2017-10-18 09:31:48 +0800

    添加快捷方式logt和查看方法的参数和返回值 ctrl+alt+space

--- chapter1/HelloWorld/app/src/main/java/com/example/helloworld/HelloWorldActivity.java
+++ chapter1/HelloWorld/app/src/main/java/com/example/helloworld/HelloWorldActivity.java
@@ -5,12 +5,12 @@ import android.os.Bundle;
 import android.util.Log;
 
 public class HelloWorldActivity extends AppCompatActivity {
-
-    private static final String TAG = "HelloWorldActivity";
-
+     //logt   ---->    ctrl+alt+space
+     private static final String TAG = "HelloWorldActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //查看方法的参数和返回值  ---->   ctrl+alt+space
         setContentView(R.layout.hello_world_layout);
         Log.d(TAG, "onCreate execute");
     }
