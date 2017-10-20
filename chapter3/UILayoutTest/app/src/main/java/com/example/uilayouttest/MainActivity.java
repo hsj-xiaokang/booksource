74c8c79aba3c89b454931d5cc10e6a5d05f0fbc9
2356899074@qq.com <2356899074@qq.com>
2017-10-20 09:46:55 +0800

    百分比布局样式

--- chapter3/UILayoutTest/app/src/main/java/com/example/uilayouttest/MainActivity.java
+++ chapter3/UILayoutTest/app/src/main/java/com/example/uilayouttest/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.uilayouttest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 百分比布局样式
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
