23cdaeccab1feaf30b1100e4a310d73a82e04813
2356899074@qq.com <2356899074@qq.com>
2017-10-23 12:38:39 +0800

    添加自己的见解

--- chapter3/UIBestPractice/app/src/main/java/com/example/uibestpractice/MainActivity.java
+++ chapter3/UIBestPractice/app/src/main/java/com/example/uibestpractice/MainActivity.java
@@ -14,42 +14,64 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private List<Msg> msgList = new ArrayList<Msg>();
-
+    //输入框
     private EditText inputText;
-
+    //发送框
     private Button send;
-
+    //比listview强的大RecyclerView
     private RecyclerView msgRecyclerView;
-
+    //适配器
     private MsgAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        //主布局文件
         setContentView(R.layout.activity_main);
+
         initMsgs(); // 初始化消息数据
+
         inputText = (EditText) findViewById(R.id.input_text);
         send = (Button) findViewById(R.id.send);
+
         msgRecyclerView = (RecyclerView) findViewById(R.id.msg_recycler_view);
+       //三大布局管理之一  布局管理器见：http://www.jianshu.com/p/6d278427b18b
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         msgRecyclerView.setLayoutManager(layoutManager);
+        /**
+         * msgList仅仅是传给MsgAdapter的构造函数引用了，此时MsgAdapter和MainActivity指向同一个msgList
+         */
         adapter = new MsgAdapter(msgList);
         msgRecyclerView.setAdapter(adapter);
+
+        //发送按钮事件
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 String content = inputText.getText().toString();
+
                 if (!"".equals(content)) {
                     Msg msg = new Msg(content, Msg.TYPE_SENT);
                     msgList.add(msg);
-                    adapter.notifyItemInserted(msgList.size() - 1); // 当有新消息时，刷新ListView中的显示
-                    msgRecyclerView.scrollToPosition(msgList.size() - 1); // 将ListView定位到最后一行
-                    inputText.setText(""); // 清空输入框中的内容
+                    //模拟回复
+                    Msg msg_ = new Msg("hello "+Math.random(), Msg.TYPE_RECEIVED);
+                    msgList.add(msg_);
+                    // 当有新消息时，刷新ListView中的显示
+                    adapter.notifyItemInserted(msgList.size() - 1);
+                    // 将ListView定位到最后一行 展示最后一条
+                    msgRecyclerView.scrollToPosition(msgList.size() - 1);
+                    // 清空输入框中的内容
+                    inputText.setText("");
                 }
             }
         });
     }
 
+    /**
+     * 初始化数据条目
+     */
     private void initMsgs() {
         Msg msg1 = new Msg("Hello guy.", Msg.TYPE_RECEIVED);
         msgList.add(msg1);
--- chapter3/UIBestPractice/app/src/main/java/com/example/uibestpractice/MsgAdapter.java
+++ chapter3/UIBestPractice/app/src/main/java/com/example/uibestpractice/MsgAdapter.java
@@ -9,10 +9,16 @@ import android.widget.TextView;
 
 import java.util.List;
 
+/**
+ * 使用RecyclerView
+ */
 public class MsgAdapter extends RecyclerView.Adapter<MsgAdapter.ViewHolder> {
-
+    //数据list
     private List<Msg> mMsgList;
 
+    /**
+     * ViewHolder类
+     */
     static class ViewHolder extends RecyclerView.ViewHolder {
 
         LinearLayout leftLayout;
@@ -23,8 +29,13 @@ public class MsgAdapter extends RecyclerView.Adapter<MsgAdapter.ViewHolder> {
 
         TextView rightMsg;
 
+        /**
+         * View view参数就是布局文件
+         * @param view
+         */
         public ViewHolder(View view) {
             super(view);
+
             leftLayout = (LinearLayout) view.findViewById(R.id.left_layout);
             rightLayout = (LinearLayout) view.findViewById(R.id.right_layout);
             leftMsg = (TextView) view.findViewById(R.id.left_msg);
@@ -32,19 +43,26 @@ public class MsgAdapter extends RecyclerView.Adapter<MsgAdapter.ViewHolder> {
         }
     }
 
+    /**
+     * 构造函数
+     * @param msgList
+     */
     public MsgAdapter(List<Msg> msgList) {
         mMsgList = msgList;
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //加载布局文件
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.msg_item, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
+        //根据position获取当前条目
         Msg msg = mMsgList.get(position);
+
         if (msg.getType() == Msg.TYPE_RECEIVED) {
             // 如果是收到的消息，则显示左边的消息布局，将右边的消息布局隐藏
             holder.leftLayout.setVisibility(View.VISIBLE);
@@ -58,6 +76,10 @@ public class MsgAdapter extends RecyclerView.Adapter<MsgAdapter.ViewHolder> {
         }
     }
 
+    /**
+     * 获取总数
+     * @return
+     */
     @Override
     public int getItemCount() {
         return mMsgList.size();
