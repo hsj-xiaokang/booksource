141ffba4241389eee1f9032883dcd9420c9cc11a
2356899074@qq.com <2356899074@qq.com>
2017-10-24 15:03:32 +0800

    广播接收器------

--- chapter5/BroadcastTest/app/src/main/java/com/example/broadcasttest/BootCompleteReceiver.java
+++ chapter5/BroadcastTest/app/src/main/java/com/example/broadcasttest/BootCompleteReceiver.java
@@ -5,11 +5,19 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
 
+/**
+ * 开机启动
+ *  广播接收器不允许开启线程或者耗时的逻辑
+ *
+ *
+ *
+ *  -----------------------------静态广播接收器-------------------------
+ */
 public class BootCompleteReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "Boot Complete", Toast.LENGTH_LONG).show();
+        Toast.makeText(context, "Boot Complete（开机启动完成！）", Toast.LENGTH_LONG).show();
     }
 
 }
--- chapter5/BroadcastTest/app/src/main/java/com/example/broadcasttest/MainActivity.java
+++ chapter5/BroadcastTest/app/src/main/java/com/example/broadcasttest/MainActivity.java
@@ -13,16 +13,34 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+/**
+ * android的四大组件之一
+ * 广播接收器
+ * 分为两大类：
+ *        有序和无序的广播
+ * 两种注册方式：
+ *         代码注册和AndroidManifest里面注册（动态广播接收器和静态广播接收器）
+ *
+ * 两种类型：
+ *        本地广播和全局广播
+ *
+ *         广播接收器不允许开启线程或者耗时的逻辑
+ */
 public class MainActivity extends AppCompatActivity {
 
+    /**--------------------------------2----------本地广播，只能动态，不能配置在Androidmanifest里面----------------
+     * 本地广播
+     * 只能动态
+     */
     private IntentFilter intentFilter;
-
     private LocalReceiver localReceiver;
-
     private LocalBroadcastManager localBroadcastManager;
 
+    /**
+     * ----------------监听网络变化的广播---------1-----动态广播接收器---
+     * @param savedInstanceState
+     */
 //    private IntentFilter intentFilter;
-//
 //    private NetworkChangeReceiver networkChangeReceiver;
 
 
@@ -30,10 +48,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        intentFilter = new IntentFilter();
-//        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
-//        networkChangeReceiver = new NetworkChangeReceiver();
-//        registerReceiver(networkChangeReceiver, intentFilter);
+
+
+        /**--------------------------------2----------本地广播，只能动态，不能配置在Androidmanifest里面----------------
+         * 本地广播
+         * 只能动态
+         */
         localBroadcastManager = LocalBroadcastManager.getInstance(this); // 获取实例
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -47,15 +67,39 @@ public class MainActivity extends AppCompatActivity {
         intentFilter.addAction("com.example.broadcasttest.LOCAL_BROADCAST");
         localReceiver = new LocalReceiver();
         localBroadcastManager.registerReceiver(localReceiver, intentFilter); // 注册本地广播监听器
+
+
+        /**
+         * ----------------监听网络变化的广播注册-----1------动态广播接收器---
+         */
+//        intentFilter = new IntentFilter();
+//        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+//        networkChangeReceiver = new NetworkChangeReceiver();
+//        registerReceiver(networkChangeReceiver, intentFilter);
     }
+
+
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-//        unregisterReceiver(networkChangeReceiver);
+
+
+        //--------------------------------2----------本地广播，只能动态，不能配置在Androidmanifest里面----------------
         localBroadcastManager.unregisterReceiver(localReceiver);
+
+        /**
+         * ---------------------监听网络变化的广播取消注册------------1-------动态广播接收器-----
+         */
+//        unregisterReceiver(networkChangeReceiver);
+
     }
 
+
+
+    /**
+     * --------------------------------2----------本地广播，只能动态，不能配置在Androidmanifest里面----------------
+     */
     class LocalReceiver extends BroadcastReceiver {
 
         @Override
@@ -65,23 +109,23 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-//    class NetworkChangeReceiver extends BroadcastReceiver {
-//
-//        @Override
-//        public void onReceive(Context context, Intent intent) {
-//            ConnectivityManager connectionManager = (ConnectivityManager)
-//                    getSystemService(Context.CONNECTIVITY_SERVICE);
-//            NetworkInfo networkInfo = connectionManager.getActiveNetworkInfo();
-//            if (networkInfo != null && networkInfo.isAvailable()) {
-//                Toast.makeText(context, "network is available",
-//                        Toast.LENGTH_SHORT).show();
-//            } else {
-//                Toast.makeText(context, "network is unavailable",
-//                        Toast.LENGTH_SHORT).show();
-//            }
-//
-//        }
-//
-//    }
+    /**
+     * --------------------重写广播接收器---------1------动态广播接收器---
+     */
+    /*class NetworkChangeReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            ConnectivityManager connectionManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connectionManager.getActiveNetworkInfo();
+            if (networkInfo != null && networkInfo.isAvailable()) {
+                Toast.makeText(context, "network is available",Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(context, "network is unavailable",Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
+    }*/
 
 }
