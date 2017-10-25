4448f64900d49ee4c63eb0c55f8240af8c03333d
2356899074@qq.com <2356899074@qq.com>
2017-10-25 09:02:22 +0800

    sharedpreference--------------

--- chapter6/SharedPreferencesTest/app/src/main/java/com/example/sharedpreferencestest/MainActivity.java
+++ chapter6/SharedPreferencesTest/app/src/main/java/com/example/sharedpreferencestest/MainActivity.java
@@ -1,18 +1,30 @@
 package com.example.sharedpreferencestest;
 
 import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+/**
+ *  1.getSharedPreferences
+ *                   说明：该方法是context里面的，第一个参数文件名称，文件都存放在/data/data/<package-name>/shared_prefs/目录下面
+ *                                              第二个参数是唯一的一个MODE_PRIVATE模式
+ *  2.getPreferences()
+ *                   说明：该方法是activity里面，第一个参数【只有一个参数，回把当前的activity类名作为文件名称】，是唯一的一个MODE_PRIVATE模式
+ *  3.PreferenceManager.getDefaultSharedPreferences()
+ *                   说明：接受一个参数【只有一个参数，回把当前的程序包名作为文件名称】
+ *                   PreferenceManager.getDefaultSharedPreferences(this).edit().putString("data","data").apply();
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Button saveData = (Button) findViewById(R.id.save_data);
         saveData.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
                 editor.apply();
             }
         });
+
         Button restoreData = (Button) findViewById(R.id.restore_data);
         restoreData.setOnClickListener(new View.OnClickListener() {
             @Override
