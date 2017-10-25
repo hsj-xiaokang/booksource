a98ae8290c493da0123851b8b25fc87064276f23
2356899074@qq.com <2356899074@qq.com>
2017-10-25 16:36:21 +0800

    ctrl+alt+o删除没有用的import

--- chapter7/RuntimePermissionTest/app/src/main/java/com/example/runtimepermissiontest/BaseActivity.java
+++ chapter7/RuntimePermissionTest/app/src/main/java/com/example/runtimepermissiontest/BaseActivity.java
@@ -1,10 +1,6 @@
 package com.example.runtimepermissiontest;
 
-import android.app.Activity;
 import android.content.pm.PackageManager;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
