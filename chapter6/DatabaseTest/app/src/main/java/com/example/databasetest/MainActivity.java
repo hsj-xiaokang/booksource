77201c94f0a9c0ad28dd9c3a849499da33e4cce7
2356899074@qq.com <2356899074@qq.com>
2017-10-25 13:53:45 +0800

    databaese 原始的方式，没有litepal

--- chapter6/DatabaseTest/app/src/main/java/com/example/databasetest/MainActivity.java
+++ chapter6/DatabaseTest/app/src/main/java/com/example/databasetest/MainActivity.java
@@ -9,6 +9,14 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+/**SQLiteOpenHelper注意：如果数据库存在直接打开，否则创建一个数据库
+ *      1.  getWritableDatabase   如果磁盘已经满了，那么出现异常exception
+ *      2.  getReadableDatabase  如果磁盘已经满了，那么返回只读的方式打开数据库
+ *
+ *
+ *
+ *
+ */
 public class MainActivity extends AppCompatActivity {
 
     private MyDatabaseHelper dbHelper;
@@ -17,18 +25,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //version大于上次才会调用onUpgrade方法
+        //没有数据库才会调用onCreate方法
         dbHelper = new MyDatabaseHelper(this, "BookStore.db", null, 2);
+
+        //创建数据库
         Button createDatabase = (Button) findViewById(R.id.create_database);
         createDatabase.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //getWritableDatabase调用的时候才去创建数据库或者更新数据库，创建或者更新之后再一次调用就不去创建数据库或者更新数据库
                 dbHelper.getWritableDatabase();
             }
         });
+
+        /**
+         * 增
+         */
         Button addData = (Button) findViewById(R.id.add_data);
         addData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //getWritableDatabase第二次添加的时候调用不去创建数据库或者更新数据库
                 SQLiteDatabase db = dbHelper.getWritableDatabase();
                 ContentValues values = new ContentValues();
                 // 开始组装第一条数据
@@ -46,6 +65,10 @@ public class MainActivity extends AppCompatActivity {
                 db.insert("Book", null, values); // 插入第二条数据
             }
         });
+
+        /**
+         * 更
+         */
         Button updateData = (Button) findViewById(R.id.update_data);
         updateData.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,17 +76,35 @@ public class MainActivity extends AppCompatActivity {
                 SQLiteDatabase db = dbHelper.getWritableDatabase();
                 ContentValues values = new ContentValues();
                 values.put("price", 10.99);
+                //第三个参数就是where部分
                 db.update("Book", values, "name = ?", new String[] { "The Da Vinci Code" });
             }
         });
+
+        /**
+         * 删
+         */
         Button deleteButton = (Button) findViewById(R.id.delete_data);
         deleteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 SQLiteDatabase db = dbHelper.getWritableDatabase();
+                //第二个参数就是where部分
                 db.delete("Book", "pages > ?", new String[] { "500" });
             }
         });
+
+        /**
+         * 查
+         * 参数列表：
+         * table            select from table_name
+         * columns          column1,column2
+         * selection        where column = value
+         * selectionArgs    [where里面的value占位值]
+         * groupBy          group by  column
+         * having           having column = value
+         * orderBy          order by column1,column2
+         */
         Button queryButton = (Button) findViewById(R.id.query_data);
         queryButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -71,6 +112,8 @@ public class MainActivity extends AppCompatActivity {
                 SQLiteDatabase db = dbHelper.getWritableDatabase();
                 // 查询Book表中所有的数据
                 Cursor cursor = db.query("Book", null, null, null, null, null, null);
+                //原生sql语句查询
+//                Cursor cursor = db.rawQuery("select * from Book where pages = ?",new String[]{"454"});
                 if (cursor.moveToFirst()) {
                     do {
                         // 遍历Cursor对象，取出数据并打印
@@ -87,6 +130,7 @@ public class MainActivity extends AppCompatActivity {
                 cursor.close();
             }
         });
+
     }
 
 }
--- chapter6/DatabaseTest/app/src/main/java/com/example/databasetest/MyDatabaseHelper.java
+++ chapter6/DatabaseTest/app/src/main/java/com/example/databasetest/MyDatabaseHelper.java
@@ -5,8 +5,15 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.widget.Toast;
 
+/**
+ * 数据文件会在data/data/<package name>/databases下面
+ */
 public class MyDatabaseHelper extends SQLiteOpenHelper {
 
+    /**
+     * 建立表语句
+     * real表示浮点型数据
+     */
     public static final String CREATE_BOOK = "create table Book ("
             + "id integer primary key autoincrement, "
             + "author text, "
@@ -21,24 +28,68 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
 
     private Context mContext;
 
-    public MyDatabaseHelper(Context context, String name,
-                            SQLiteDatabase.CursorFactory factory, int version) {
+    public MyDatabaseHelper(Context context,
+                            String name,//数据库名称
+                            SQLiteDatabase.CursorFactory factory,
+                            int version//版本
+      ) {
         super(context, name, factory, version);
         mContext = context;
     }
 
+    /**
+     * 建立数据库和数据表
+     * 数据库存在就不会再一次创建了调用该方法了
+     * @param db
+     */
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_BOOK);
         db.execSQL(CREATE_CATEGORY);
-        Toast.makeText(mContext, "Create succeeded", Toast.LENGTH_SHORT).show();
+        Toast.makeText(mContext, "onCreate succeeded", Toast.LENGTH_SHORT).show();
     }
 
+    /**
+     * version大于上一次就会执行该方法
+     * @param db
+     * @param oldVersion
+     * @param newVersion
+     */
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("drop table if exists Book");
         db.execSQL("drop table if exists Category");
         onCreate(db);
+        Toast.makeText(mContext, "onUpgrade succeeded", Toast.LENGTH_SHORT).show();
     }
 
 }
+///*输入 abd shell 进入设备控制台*/
+//C:\Users\dudon>adb shell
+//
+///*使用 cd 命令进入到数据库存放目录下*/
+//        root@vbox86p:/ # cd /data/data/com.example.dudon.databasetest/databases/
+//
+///*使用 ls 命令查看该目录里的文件*/
+//        root@vbox86p: # ls
+//
+//        BookStore.db
+///*db-journal是为了让数据库能够支持事务而产生的临时日志文件*/
+//        BookStore.db-journal
+//
+///*借助 sqlite 命令打开数据库，输入sqlite3加数据库名即可*/
+//        root@vbox86p: # sqlite3 BookStore.db                                                              <
+//        SQLite version 3.7.11 2012-03-20 11:35:50
+//        Enter ".help" for instructions
+//        Enter SQL statements terminated with a ";"
+//
+///*输入 .table 命令，查看数据库中的表*/
+//        sqlite> .table
+//        android_metadata  book
+//
+///*输入 .schema 命令查看建表语句*/
+//        sqlite> .schema
+//        CREATE TABLE android_metadata (locale TEXT);
+//        CREATE TABLE book (id  integer PRIMARY KEY Autoincrement ,
+//        author text ,price real ,pages integer,name text );
+//
