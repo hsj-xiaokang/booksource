e0d6879287140cc8a7366c348896602ced9f72bc
2356899074@qq.com <2356899074@qq.com>
2017-10-25 17:10:23 +0800

    优化修改-----

--- chapter7/ContactsTest/app/src/main/java/com/example/contactstest/MainActivity.java
+++ chapter7/ContactsTest/app/src/main/java/com/example/contactstest/MainActivity.java
@@ -18,6 +18,8 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final static int REQUEST_CODE_1 = 1;
+
     ArrayAdapter<String> adapter;
 
     List<String> contactsList = new ArrayList<>();
@@ -26,31 +28,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         ListView contactsView = (ListView) findViewById(R.id.contacts_view);
+
         adapter = new ArrayAdapter<String>(this, android.R.layout. simple_list_item_1, contactsList);
+
         contactsView.setAdapter(adapter);
+
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.READ_CONTACTS }, 1);
+            ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.READ_CONTACTS }, REQUEST_CODE_1);
         } else {
             readContacts();
         }
     }
 
+    /**
+     * 获取联系人
+     */
     private void readContacts() {
+
         Cursor cursor = null;
+
         try {
             // 查询联系人数据
             cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
+
             if (cursor != null) {
                 while (cursor.moveToNext()) {
                     // 获取联系人姓名
                     String displayName = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
                     // 获取联系人手机号
                     String number = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+
                     contactsList.add(displayName + "\n" + number);
                 }
+                //通知数据改变
                 adapter.notifyDataSetChanged();
             }
+
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -60,11 +75,18 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * 权限
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         switch (requestCode) {
-            case 1:
+            case REQUEST_CODE_1:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //获取联系人
                     readContacts();
                 } else {
                     Toast.makeText(this, "You denied the permission", Toast.LENGTH_SHORT).show();
