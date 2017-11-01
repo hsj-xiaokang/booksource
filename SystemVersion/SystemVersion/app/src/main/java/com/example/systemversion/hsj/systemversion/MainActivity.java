90c656f9441e1d1888aee7a00bd33865106cbee6
2356899074@qq.com <2356899074@qq.com>
2017-11-01 14:26:55 +0800

    优化

--- SystemVersion/SystemVersion/app/src/main/java/com/example/systemversion/hsj/systemversion/MainActivity.java
+++ SystemVersion/SystemVersion/app/src/main/java/com/example/systemversion/hsj/systemversion/MainActivity.java
@@ -8,6 +8,10 @@ import android.widget.TextView;
 
 import com.example.systemversion.hsj.systemversion.utils.ObjectUtils;
 
+/**
+ * @author heshengjin
+ * @date 2017-11-01
+ */
 public class MainActivity extends AppCompatActivity {
 
     private TextView versionName;
@@ -18,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        versionName = (TextView) this.findViewById(R.id.versionName);
+        versioncode = (TextView) findViewById(R.id.versioncode);
         //获取versionInfo
         String[] versionInfo = getVersionInfo();
         if(!ObjectUtils.isEmpty(versionInfo)){
@@ -28,12 +35,12 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * 获取系统的信息相关
+     * 正式项目考虑封装成一个util工具
+     * 使用随时随地获取Context
      * @return
      */
     private String[] getVersionInfo() {
         //系统版本
-        versionName = (TextView) this.findViewById(R.id.versionName);
-        versioncode = (TextView) findViewById(R.id.versioncode);
         String[] versionInfo = null;
 
         PackageManager pm = MainActivity.this.getPackageManager();
@@ -42,14 +49,24 @@ public class MainActivity extends AppCompatActivity {
             pi = pm.getPackageInfo(MainActivity.this.getPackageName(), 0);
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
+            throw new NullPointerException(getFailureMessage());
         }
         if (!ObjectUtils.isEmpty(pi)) {
             versionInfo = new String[2];
             versionInfo[0] = pi.versionName;
             versionInfo[1] = pi.versionCode+"";
         } else {
-            throw new NullPointerException("PackageInfo get failure!");
+            throw new NullPointerException(getFailureMessage());
         }
         return versionInfo;
+    }
+
+    /**
+     * 获取失败的信息
+     * 以后实际的项目就配在string.xml文件里面
+     * @return
+     */
+    private String getFailureMessage(){
+        return getString(R.string.PackageInfo_get_fail);
     }
 }
