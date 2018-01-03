f3d7e9ba8386214d5886a704476d9a46a6710b8b
2356899074@qq.com <2356899074@qq.com>
2018-01-03 09:31:07 +0800

    最佳实践

---
+++ chapter13/BeastActionTest/app/src/main/java/com/example/hsj/beastactiontest/utils/DateUtils.java
@@ -0,0 +1,26 @@
+package com.example.hsj.beastactiontest.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * Created by hsj on 2017/10/20.
+ */
+
+public final class DateUtils {
+    //格式化时间
+    private final static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    static {
+        //设置正确的时区
+        sdf.setTimeZone(TimeZone.getTimeZone("Asia/Shanghai"));
+    }
+
+    /**
+     * 格式化得到当前时间
+     * @return
+     */
+    public static String getCurrentTimeFormate(){
+        return sdf.format(new Date());
+    }
+}
