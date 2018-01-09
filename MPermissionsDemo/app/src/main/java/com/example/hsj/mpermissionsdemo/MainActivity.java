3710a6d312efd7f227272edf51d2744392df4d1b
2356899074@qq.com <2356899074@qq.com>
2018-01-09 17:18:26 +0800

    权限demo源码-MPermissions

---
+++ MPermissionsDemo/app/src/main/java/com/example/hsj/mpermissionsdemo/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.hsj.mpermissionsdemo;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.zhy.m.permission.MPermissions;
+import com.zhy.m.permission.PermissionDenied;
+import com.zhy.m.permission.PermissionGrant;
+import com.zhy.m.permission.ShowRequestPermissionRationale;
+
+/**
+ * 高版本的gradle报错：
+ * http://blog.csdn.net/sxk874890728/article/details/78367726
+ */
+public class MainActivity extends AppCompatActivity
+{
+
+    private Button mBtnSdcard, mBtnCallPhone;
+    private static final int REQUECT_CODE_SDCARD = 2;
+    private static final int REQUECT_CODE_CALL_PHONE = 3;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mBtnSdcard = (Button) findViewById(R.id.id_btn_sdcard);
+        mBtnCallPhone = (Button) findViewById(R.id.id_btn_callphone);
+
+        mBtnSdcard.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+                if (!MPermissions.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE, REQUECT_CODE_SDCARD))
+                {
+                    MPermissions.requestPermissions(MainActivity.this, REQUECT_CODE_SDCARD, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                }
+            }
+        });
+
+        mBtnCallPhone.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                MPermissions.requestPermissions(MainActivity.this, REQUECT_CODE_CALL_PHONE, Manifest.permission.CALL_PHONE);
+            }
+        });
+    }
+
+    @ShowRequestPermissionRationale(REQUECT_CODE_SDCARD)
+    public void whyNeedSdCard()
+    {
+        Toast.makeText(this, "I need write news to sdcard!", Toast.LENGTH_SHORT).show();
+        MPermissions.requestPermissions(MainActivity.this, REQUECT_CODE_SDCARD, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
+    {
+        MPermissions.onRequestPermissionsResult(this, requestCode, permissions, grantResults);
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+
+    @PermissionGrant(REQUECT_CODE_SDCARD)
+    public void requestSdcardSuccess()
+    {
+        Toast.makeText(this, "GRANT ACCESS SDCARD!", Toast.LENGTH_SHORT).show();
+    }
+
+    @PermissionDenied(REQUECT_CODE_SDCARD)
+    public void requestSdcardFailed()
+    {
+        Toast.makeText(this, "DENY ACCESS SDCARD!", Toast.LENGTH_SHORT).show();
+    }
+
+
+    @PermissionGrant(REQUECT_CODE_CALL_PHONE)
+    public void requestCallPhoneSuccess()
+    {
+        Toast.makeText(this, "GRANT ACCESS SDCARD!", Toast.LENGTH_SHORT).show();
+    }
+
+    @PermissionDenied(REQUECT_CODE_CALL_PHONE)
+    public void requestCallPhoneFailed()
+    {
+        Toast.makeText(this, "DENY ACCESS SDCARD!", Toast.LENGTH_SHORT).show();
+    }
+
+
+}
